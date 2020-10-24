bc20c7ab73924d16b7671cf97689e50c1a012661
yuxuan wang <2879296442@qq.com>
2020-10-24 14:25:55 +0100

    Exercise 2 completed

--- components/exercises/02_embeddedVariables.js
+++ components/exercises/02_embeddedVariables.js
@@ -1,10 +1,45 @@
 import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  const course = "MSc Computing - Modules table.";
+  const modules = [
+    {
+      name: "DevOps",
+      noLectures: 2,
+      noPracticals: 3
+    },
+    {
+      name: "Enterprise Web Dev",
+      noLectures: 3,
+      noPracticals: 2
+    }
+  ];
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>{course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].noLectures}</td>
+            <td>{modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].noLectures}</td>
+            <td>{modules[1].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
