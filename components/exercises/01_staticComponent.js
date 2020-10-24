b4b976c128325ade84e4c8d98dc01d8d3d78a9f4
yuxuan wang <2879296442@qq.com>
2020-10-24 14:16:29 +0100

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,7 +3,30 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>HDip Computer Science - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>DevOps</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Enterprise Web Dev</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
